895716da3155f4ff847131f9b1663fb346fd3d44
Rishita Hariyani <rhariyani@cnm.edu>
2019-06-20 13:02:52 -0600

    3 assigmnet completed

---
+++ conditional and functions.js
@@ -0,0 +1,92 @@
+/*
+* Conditonals, Functions, Loops and Scope
+ */
+/* Equals
+let equals = 1 === 1;
+// Greater than
+let greaterThan = 5 > 1;
+
+// Less Than
+let lessThan = 2 < 10;
+
+//Greater Than Equals
+let greaterThanEq = 5 >= 5;
+
+//Less Than Equals
+let lessThanEq = 4 <=9;
+
+// Not Equals
+let notEquals = 5 !==2;
+ */
+/*let storeA = 1.40;
+let storeB = 4.40;
+
+
+function CompareStorePrices( storeA, storeB) {
+	let StoreAIsLower = storeA < storeB;
+	if(StoreAIsLower) {
+		console.log("Store A has a lower price.")
+	} else if(storeB < storeA) {
+		console.log("Store B has a lower price.")
+	} else {
+		console.log("Their price are eaqual")
+	}
+}
+
+//CompareStorePrices(10,5);
+//CompareStorePrices(7,10);
+
+ */
+
+ /*function squareNum (number) {
+	 let squared = number * number;
+	 return squared;
+
+ }
+ let squareNumber = squareNum(7);
+console.log(squareNumber);
+
+  */
+
+ /*let x = 10;
+  function addNumbers (n,m,x){
+	  console.log(x);
+	  let b;
+	  if(1===1)
+	  {
+	   b=8;
+	  }
+	  console.log(b)
+  	return n+m;
+  }
+  addNumbers(2,3,8);
+  */
+ /*
+ //              0 1 2 3 4
+ let ourArray = [1, 2, 3, 4, ['a','b','c']];
+
+// let newArr = ourArray[4];
+// console.log(newArr);
+//loop will help to print all array one time
+let arrlen = ourArray.length;
+for (let i = 0; i<arrlen; i++) {
+	//console.log("i is equal to:", + i);
+	console.log(ourArray[i]);
+
+	for (let j =0; j<=10; j++)
+	{
+		console.log(' j is equal to:' + j);
+	}
+
+}
+
+  */
+
+ //while loop
+ let x=0;
+while (x<10)
+{
+	console.log('Ran');
+	x=x +1;
+}
+
